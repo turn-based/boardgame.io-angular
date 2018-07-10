608c08e2638d2ba28f7967e48a7f278094e95544
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2018-07-10 15:17:55 +0300

    add server verification

--- server/start.ts
+++ server/start.ts
@@ -2,20 +2,52 @@ import { Server } from 'boardgame.io/server';
 import KoaHelmet from 'koa-helmet';
 import mount from 'koa-mount';
 import { TicTacToe } from '../shared/games/tic-tac-toe';
+import Router from 'koa-router';
+import Koa from 'koa';
+import jwt from 'koa-jwt';
+import { koaJwtSecret } from 'jwks-rsa';
+import cors from '@koa/cors';
 
 const PORT = process.env.PORT || 8000;
 
 const server = Server({games: [TicTacToe]});
 
 if (process.env.NODE_ENV !== 'development') {
-    server.app.use(KoaHelmet());
+  server.app.use(KoaHelmet());
 }
-
 server.app.use(mount('/api/v1', server.api));
 
+
+const jwtCheck = jwt({
+  secret: koaJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://amitport.auth0.com/.well-known/jwks.json'
+  }) as any,
+  audience: 'https://api.turn-based.com',
+  issuer: 'https://amitport.auth0.com/',
+  algorithms: ['RS256']
+});
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/ping', (ctx) => {
+  ctx.body = ctx.state.user;
+});
+
+app
+  .use(cors())
+  .use(jwtCheck)
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+server.app.use(mount('/api-authorized', app));
+
 (async () => {
-    await server.db.connect();
-    server.app.listen(PORT, () => {
-        console.log(`Serving at: http://localhost:${PORT}/`);
-    });
+  await server.db.connect();
+  server.app.listen(PORT, () => {
+    console.log(`Serving at: http://localhost:${PORT}/`);
+  });
 })();
--- src/app/lobby/lobby.component.ts
+++ src/app/lobby/lobby.component.ts
@@ -1,31 +1,40 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { SERVER_ORIGIN } from '../site-config';
 
 @Component({
   template: `
-    <div style="padding: 16px" [ngSwitch]="auth.isAuthenticated()">
-      <button *ngSwitchCase="false" mat-raised-button (click)="login()">login</button>
+    <div style="padding: 16px" fxLayout="column" fxLayoutGap="16px">
+      <div [ngSwitch]="auth.isAuthenticated()">
+        <button *ngSwitchCase="false" mat-raised-button (click)="login()">login</button>
 
-      <ng-container *ngSwitchCase="true">
-        <ng-container *ngIf="(auth.getProfile() | async) as profile">
-          <mat-card style="width: 280px;">
-            <mat-card-title-group>
-              <mat-card-title>{{ profile.nickname }}</mat-card-title>
-              <mat-card-subtitle>{{ profile.email }}</mat-card-subtitle>
-              <img mat-card-md-image [src]="profile.picture">
-            </mat-card-title-group>
-            <mat-card-actions>
-              <button mat-raised-button (click)="logout()">logout</button>
-            </mat-card-actions>
-          </mat-card>
+        <ng-container *ngSwitchCase="true">
+          <ng-container *ngIf="(auth.getProfile() | async) as profile">
+            <mat-card style="width: 280px;">
+              <mat-card-title-group>
+                <mat-card-title>{{ profile.nickname }}</mat-card-title>
+                <mat-card-subtitle>{{ profile.email }}</mat-card-subtitle>
+                <img mat-card-md-image [src]="profile.picture">
+              </mat-card-title-group>
+              <mat-card-actions>
+                <button mat-raised-button (click)="logout()">logout</button>
+              </mat-card-actions>
+            </mat-card>
+          </ng-container>
         </ng-container>
-      </ng-container>
+      </div>
+
+      <button mat-raised-button (click)="authorizedPing()">authorized ping</button>
+      <mat-card *ngIf="message">{{ message | json}}</mat-card>
     </div>
 
   `
 })
 export class LobbyComponent {
-  constructor(public auth: AuthService) {
+  message: any;
+
+  constructor(public auth: AuthService, private http: HttpClient) {
   }
 
   login() {
@@ -36,4 +45,14 @@ export class LobbyComponent {
     this.auth.logout();
   }
 
+  authorizedPing() {
+    this.http.get(`${SERVER_ORIGIN}/api-authorized/ping`, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${localStorage.getItem('access_token')}`)
+    })
+      .subscribe(
+        data => this.message = data,
+        error => this.message = error
+      );
+  }
 }
