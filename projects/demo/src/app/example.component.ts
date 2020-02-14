96fde82c13f87677bc2196c72bcc7644098956c7
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2020-02-14 22:17:23 +0200

    small refactor

--- projects/demo/src/app/example.component.ts
+++ projects/demo/src/app/example.component.ts
@@ -1,26 +1,18 @@
 import { Component, Injector } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Example } from './examples';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   template: `
-    <ng-container *ngIf="component; else noConfig">
-      <ng-container *ngComponentOutlet="component; injector: injector;"></ng-container>
-    </ng-container>
-    <ng-template #noConfig>Could not find game config</ng-template>
+    <ng-container *ngComponentOutlet="component$ | async; injector: injector;"></ng-container>
   `,
 })
 export class ExampleComponent {
-  component: null | object = null;
+  component$: Observable<object>;
 
   constructor(route: ActivatedRoute, public injector: Injector) {
-    route.data.subscribe(({example}: { example: Example }) => {
-      const p = Object.getPrototypeOf(example);
-      if (p != null && p !== Object.prototype) {
-        this.component = example;
-      } else {
-        this.component = null;
-      }
-    });
+    this.component$ = route.data.pipe(map(({example}: { example: Example }) => example));
   }
 }
--- projects/demo/src/app/examples/tic-tac-toe/index.ts
+++ projects/demo/src/app/examples/tic-tac-toe/index.ts
@@ -6,6 +6,4 @@ import { MultiplayerComponent } from './multiplayer.component';
 export default [
   SingleplayerComponent,
   MultiplayerComponent,
-  {sectionId: 'Tic-Tac-Toe', exampleName: 'Authenticated'},
-  {sectionId: 'Tic-Tac-Toe', exampleName: 'Spectator'},
 ];
